db0ae37b0e7b5ef0241ec9ce14979b05fc59b462
urbapali <urbapali@gmail.com>
2025-05-10 11:05:01 +0200

    kesz

--- script.js
+++ script.js
@@ -14,6 +14,7 @@ document.getElementById(components.input).addEventListener("input", () => {
 
 document.getElementById(components.button).addEventListener("click", async () => {
   const data = await getPokemonByName(document.getElementById(components.input).value);
+  console.log(data);
   if (data) {
     clearContainer();
     createPokemonImage(data);
@@ -21,15 +22,18 @@ document.getElementById(components.button).addEventListener("click", async () =>
 });
 
 async function getPokemonByName(pokemon_name) {
-  const response = await fetch(OPTIONS.api + pokemon_name);
-  if (!response.ok) return alert("Nincs ilyen pokemon!");
-  return await response.json();
+  try {
+    return await (await fetch(OPTIONS.api + pokemon_name)).json();
+  } catch (error) {
+    console.log("Hiba történt: " + error);
+  }
 }
 
 function createPokemonImage(data) {
   document.getElementById(components.container).append(
     Object.assign(document.createElement("img"), {
       src: data.sprites.front_default,
+      alt: data.name,
     })
   );
 }
